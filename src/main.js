41638dd306ac6825beada4beb3ca10a7607b1e14
Anna Bush <annabushsoftware@gmail.com>
2022-12-23 15:47:34 -0500

    switch to bootstrap scss and add variables

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import App from './App.vue'
 import router from './router'
 import BootstrapVue3 from 'bootstrap-vue-3'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import './styles/main.scss'
 
 const app = createApp(App)
