4deacb5c0028be61625a9289d3d7870e260ce628
Anna Bush <annabushsoftware@gmail.com>
2022-12-29 17:57:15 -0500

    schedule table

--- src/components/ScheduleItem.vue
+++ src/components/ScheduleItem.vue
@@ -1,11 +1,27 @@
 <template>
     <div>
-        <h2>Schedule Item</h2>
+        <h2>{{ this.hour }}:{{ this.min }}</h2>
     </div>
 </template>
 
 <script>
 export default {
-
+    props: {
+        day: {
+            type: String,
+            required: true,
+            default: 'Item'
+        },
+        hour: {
+            type: String,
+            required: false,
+            default: '12'
+        },
+        min: {
+            type: String,
+            required: false,
+            default: '00'
+        }
+    }
 }
 </script>
--- src/views/ScheduleDemoView.vue
+++ src/views/ScheduleDemoView.vue
@@ -1,17 +1,62 @@
 <template>
     <div>
         <h1>Schedule Demo View</h1>
-        <ScheduleItem />
-        <ScheduleItem />
+        <b-table fixed bordered table-variant="light" :items="this.tableData" :fields="this.tableFields">
+            <template #cell()="data">
+                <ScheduleItem :day="data.value.dayAbbr" :hour="data.value.hour" :min="data.value.min" />
+            </template>
+        </b-table>
     </div>
 </template>
 
 <script>
 import ScheduleItem from '../components/ScheduleItem.vue'
 
+const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
 export default {
     components: {
         ScheduleItem
+    },
+    data() {
+        return {
+            tableFields: days,
+            tableData: [
+                this.getTableRow(),
+                this.getTableRow()
+            ]
+            /*
+            [ 
+                {
+                    sun: {},
+                    mon: {},
+                    tues: {}
+                },
+                ...
+            ]
+            */
+        }
+    },
+    // computed: {
+        
+    // },
+    // mounted() {
+    //     console.log(this.tableData)
+    // },
+    methods: {
+        getTableRow() {
+            var rowObj = {};
+            days.forEach(day => {
+                rowObj[day] = {
+                    dayAbbr: day,
+                    hour: '12',
+                    min: '00',
+                    active: false
+                }
+            })
+            return rowObj;
+        },
     }
+
 }
 </script>
