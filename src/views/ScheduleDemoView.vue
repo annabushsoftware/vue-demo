d3db742fd06119fa9b253ad9dec50203d8cff538
Anna Bush <annabushsoftware@gmail.com>
2022-12-29 14:43:52 -0500

    remove unnecessary styling, add schedule demo view

---
+++ src/views/ScheduleDemoView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Schedule Demo View</h1>
+        <ScheduleItem />
+        <ScheduleItem />
+    </div>
+</template>
+
+<script>
+import ScheduleItem from '../components/ScheduleItem.vue'
+
+export default {
+    components: {
+        ScheduleItem
+    }
+}
+</script>
