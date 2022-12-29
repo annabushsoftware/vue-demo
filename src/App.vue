8672e8fa2056229ef61620ed6486816bb487e62b
Anna Bush <annabushsoftware@gmail.com>
2022-12-29 16:36:16 -0500

    move navbar to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import CustomNavbar from './components/CustomNavbar.vue';
 </script>
 
 <template>
-  <b-navbar toggleable="lg" type="light" variant="info" fixed="top">
-    <b-navbar-brand href="#">Vue Demo App</b-navbar-brand>
-  
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-  
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/about">About</b-nav-item>
-        <b-nav-item to="/schedule-demo">Schedule Demo</b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+  <CustomNavbar />
 
   <div class="app-body">
     <RouterView />
--- src/components/CustomNavbar.vue
+++ src/components/CustomNavbar.vue
@@ -1,16 +1,15 @@
 <template>
-    <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-navbar-brand href="#">Vue Demo App</b-navbar-brand>
+    <b-navbar toggleable="lg" type="light" variant="info" fixed="top">
+        <b-navbar-brand href="#">Vue Demo App</b-navbar-brand>
     
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item href="#">Link</b-nav-item>
-                    <b-nav-item href="#" disabled>Disabled</b-nav-item>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item to="/">Home</b-nav-item>
+                <b-nav-item to="/about">About</b-nav-item>
+                <b-nav-item to="/schedule-demo">Schedule Demo</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
 </template>
