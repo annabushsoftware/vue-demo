2e5f2b0a695483da33e4a85ebe770b4a5bbf6f30
Anna Bush <annabushsoftware@gmail.com>
2022-12-31 17:03:33 -0500

    add popover, never closes

--- src/views/ScheduleDemoView.vue
+++ src/views/ScheduleDemoView.vue
@@ -3,8 +3,15 @@
         <h1>Schedule Demo View</h1>
         <b-table fixed bordered table-variant="light" :fields="this.tableFields" :items="this.tableData">
             <template #cell()="data">
-                <ScheduleItem :day="data.value.dayAbbr" :hour="data.value.hour" :min="data.value.min"
-                    @click="handleCellClick(data.value.dayAbbr, data.item.rowKey)" />
+                <div :id="this.getCellRef(data.value.dayAbbr, data.item.rowKey)"
+                     :ref="this.getCellRef(data.value.dayAbbr, data.item.rowKey)">
+                    <ScheduleItem :day="data.value.dayAbbr" :hour="data.value.hour" :min="data.value.min"
+                        @click="handleCellClick(data.value.dayAbbr, data.item.rowKey)" />
+                </div>
+                <b-popover :target="this.$refs[this.getCellRef(data.value.dayAbbr, data.item.rowKey)]" triggers="click"
+                    placement="bottom" :ref="this.getPopoverRef(data.value.dayAbbr, data.item.rowKey)">
+                    Popover content
+                </b-popover>
             </template>
         </b-table>
     </div>
@@ -22,7 +29,8 @@ export default {
     data() {
         return {
             tableFields: days,
-            activeCell: null
+            activeCell: null,
+            placeholderProp: false
         }
     },
     computed: {
@@ -31,6 +39,13 @@ export default {
                 this.getTableRow(0),
                 this.getTableRow(1)
             ]
+        },
+        activeDayRow() {
+            if(this.activeCell) {
+                return this.activeCell.split('-');
+            } else {
+                return null;
+            }
         }
     },
     // mounted() {
@@ -38,7 +53,10 @@ export default {
     // },
     methods: {
         getTableRow(rowIndex) {
-            var rowObj = {};
+            var rowObj = {
+                rowKey: rowIndex
+            };
+            
             days.forEach(day => {
                 rowObj[day] = {
                     dayAbbr: day,
@@ -46,22 +64,37 @@ export default {
                     min: '00'
                 }
             });
-            rowObj.rowKey = rowIndex;
-            
-            if(this.activeCell) {
-                var dayRow = this.activeCell.split('-');
-                if(dayRow[1] === rowIndex.toString()) {
-                    rowObj._cellVariants = { [dayRow[0]]: 'info' }
-                }
+
+            if (this.activeCell && this.activeDayRow[1] === rowIndex.toString()) {
+                rowObj._cellVariants = { [this.activeDayRow[0]]: 'info' }
             }
+
             return rowObj;
+        },
+        getCellRef(dayAbbr, rowKey) {
+            return `schedule-item-${dayAbbr}-${rowKey}`;
+        },
+        getPopoverRef(dayAbbr, rowKey) {
+            return `popover-${dayAbbr}-${rowKey}`;
         },
         handleCellClick(dayAbbr, rowKey) {
             var newActiveCell = `${dayAbbr}-${rowKey}`;
-            if(this.activeCell && this.activeCell === newActiveCell) {
+            
+            if (this.activeCell && this.activeCell === newActiveCell) {
                 this.activeCell = null;
             } else {
+                if(this.activeCell) {
+                    // close old popover
+                    var ref = this.getPopoverRef(this.activeDayRow[0], this.activeDayRow[1]);
+                    console.log('ref: ', this.$refs[ref])
+                    // this.$refs[ref].$emit('hidden');
+                }
+                
                 this.activeCell = newActiveCell;
+                // this.placeholderProp = true;
+                // this.$refs[].$emit('hide.bs.popover')
+
+                // this.$refs[this.getPopoverRef(dayAbbr, rowKey)].$emit('show');
             }
         }
     }
