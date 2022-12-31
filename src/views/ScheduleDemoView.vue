6eabeeaa4046540a45fc9e6cead00e5d104d28c4
Anna Bush <annabushsoftware@gmail.com>
2022-12-31 15:53:42 -0500

    selectable table cells

--- src/views/ScheduleDemoView.vue
+++ src/views/ScheduleDemoView.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
         <h1>Schedule Demo View</h1>
-        <b-table fixed bordered table-variant="light" :items="this.tableData" :fields="this.tableFields">
+        <b-table fixed bordered table-variant="light" :fields="this.tableFields" :items="this.tableData">
             <template #cell()="data">
-                <ScheduleItem :day="data.value.dayAbbr" :hour="data.value.hour" :min="data.value.min" />
+                <ScheduleItem :day="data.value.dayAbbr" :hour="data.value.hour" :min="data.value.min"
+                    @click="handleCellClick(data.value.dayAbbr, data.item.rowKey)" />
             </template>
         </b-table>
     </div>
@@ -21,41 +22,48 @@ export default {
     data() {
         return {
             tableFields: days,
-            tableData: [
-                this.getTableRow(),
-                this.getTableRow()
-            ]
-            /*
-            [ 
-                {
-                    sun: {},
-                    mon: {},
-                    tues: {}
-                },
-                ...
-            ]
-            */
+            activeCell: null
         }
     },
-    // computed: {
-        
-    // },
+    computed: {
+        tableData() {
+            return [
+                this.getTableRow(0),
+                this.getTableRow(1)
+            ]
+        }
+    },
     // mounted() {
     //     console.log(this.tableData)
     // },
     methods: {
-        getTableRow() {
+        getTableRow(rowIndex) {
             var rowObj = {};
             days.forEach(day => {
                 rowObj[day] = {
                     dayAbbr: day,
                     hour: '12',
-                    min: '00',
-                    active: false
+                    min: '00'
                 }
-            })
+            });
+            rowObj.rowKey = rowIndex;
+            
+            if(this.activeCell) {
+                var dayRow = this.activeCell.split('-');
+                if(dayRow[1] === rowIndex.toString()) {
+                    rowObj._cellVariants = { [dayRow[0]]: 'info' }
+                }
+            }
             return rowObj;
         },
+        handleCellClick(dayAbbr, rowKey) {
+            var newActiveCell = `${dayAbbr}-${rowKey}`;
+            if(this.activeCell && this.activeCell === newActiveCell) {
+                this.activeCell = null;
+            } else {
+                this.activeCell = newActiveCell;
+            }
+        }
     }
 
 }
